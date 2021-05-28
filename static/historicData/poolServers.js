e399152ebc5b5492cd6cf7cf970851421afdf954
tiago <tiagocc0@gmail.com>
2021-05-28 10:45:31 -0300

    "Automatic commit from Tiago's QtFlexPoolCMD."

--- static/historicData/poolServers.js
+++ static/historicData/poolServers.js
@@ -1,1 +1,1 @@
-let poolServers={ 'Asia': [251,917.48],'Australia': [197,755.84],'Europe': [3941,14633.69],'South America': [277,989.24],'USA': [2403,9118.90] }
+let poolServers={ 'Asia': [251,917.48],'Australia': [197,755.84],'Europe': [3941,14633.69],'South America': [277,989.24],'USA': [2404,9121.53] }
